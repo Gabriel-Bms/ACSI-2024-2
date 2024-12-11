085e532444b3b59455d49ad18ce9d5d02845adcc
Gabriel-Bms <gabriel.marcos@upch.pe>
2024-12-11 04:01:17 +0000

    new front

--- streamlit_app.py
+++ streamlit_app.py
@@ -599,15 +599,15 @@ def main_page():
     
     st.markdown("""
         <style>
-        @import url('https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet');
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet');
         @import url('https://fonts.googleapis.com/css2?family=Bree+Serif&family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet');
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet');
         .title {
-            font-size: 80px;
+            font-size: 9gi0px;
             font-family: 'Poppins', sans-serif; 
             color: #FFFFFF;
             text-align: left;
-            font-weight: 800; 
+            font-weight: 900; 
             margin-bottom: 5px;
         }
         .subtitle {
